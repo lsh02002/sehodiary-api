72a19caf3f9231f1844092d9fc039f876038fe5c
lsh02002 <lsh02002@hanmail.net>
2026-03-21 00:55:49 +0900

    add jpaauditingconfig modules

---
+++ src/main/java/com/shop/sehodiary_api/config/security/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.shop.sehodiary_api.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
